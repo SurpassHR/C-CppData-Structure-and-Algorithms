ee750e48acb258abda51d8fca5aa024ec3af4ed5
SurpassHR <hu.rui0530@gmail.com>
2023-08-20 21:28:22 +0800

    23-08-20 21:28

--- LeetCode/23-07-03/35. 搜索插入位置.cpp
+++ LeetCode/23-07-03/35. 搜索插入位置.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
+    int searchInsert(const vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size();
         int mid = (left + right) / 2;
         while (left < mid && right > mid && nums[mid] != target) {
             if (target < nums[mid]) {
-                right == mid;
+                right = mid;
             } else {
-                left == mid;
+                left = mid;
             }
             mid = (left + right) / 2;
         }
