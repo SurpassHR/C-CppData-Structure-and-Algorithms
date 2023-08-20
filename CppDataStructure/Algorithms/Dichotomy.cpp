c35ed5d4826ab3eddac3f2dc1d52de662c342ad8
SurpassHR <hu.rui0530@gmail.com>
2023-08-20 18:05:49 +0800

    change commit strategy

--- CppDataStructure/Algorithms/Dichotomy.cpp
+++ CppDataStructure/Algorithms/Dichotomy.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -31,3 +32,36 @@ int main() {
     return 0;
 
 }
+*/
+#include <cstdio>
+int main() {
+    int left, right, mid;
+    int a[] = { 1,3,5,6,7,8,10,30,56,87,98 };
+    left = 0;
+    right = sizeof(a) / sizeof(a[0]);
+    mid = (left + right) / 2;
+    printf("a number:");
+    int num;
+    scanf("%d", &num);
+    while (left < right) {
+        if (num == a[mid]) {
+            printf("%d\n", mid);
+            break;
+        } else if (num > a[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        mid = (left + right) / 2;
+    }
+    printf("%d\n", -1);
+    return 0;
+}
+
+// #include <stdio.h>
+//
+// int main() {
+//     printf("hello, world");
+//
+//     return 0;
+// }
