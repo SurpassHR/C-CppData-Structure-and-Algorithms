c59c149437bf2891dddc88d64188ead7051307f4
hurui 30045134 <hurui51@huawei.com>
2024-05-20 09:07:36 +0800

    daily test cpp

---
+++ CppTestingProg/TestConst.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int *const p1 = nullptr; // typeof(p1)
+    const int p2 = 0;
+    const int *p3 = nullptr;
+    const int *const p4 = nullptr;
+
+    return 0;
+}
